bb0ee27edd3f67dc99927927d80e8f7f69b6bbb5
DUDUKA VENKAT SAI <136826767+sai2963@users.noreply.github.com>
2023-09-07 19:46:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 let name = "Sai";
 let age= 20;
 let hobbies = ['coding','reading','playing','eating', 'learning ' ]
-alert(hobbies[0])
-alert(hobbies[-1])
-alert(hobbies[1])
-alert(hobbies[2])
-alert(hobbies[3]) 
-alert(hobbies[4])
+
