e56920ee3d1ef19f993af8e1883f0b30b5b91d1b
DUDUKA VENKAT SAI <136826767+sai2963@users.noreply.github.com>
2023-09-09 18:56:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ let name = "Sai";
 let age= 20;
 let hobbies = ['coding','reading','playing','eating', 'learning ' ]
 console.log(hobbies[0] )
-console.log(hobbies[1] ) 
+console.log(hobbies[1] )
+console.log(hobbies[2] ) 
