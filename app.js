f0aa4b8d1ab372129f752d7977b37594e2e8f485
sai2963 <venkatsai0629@gmail.com>
2023-10-07 07:02:37 -0700

    file update

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 let name = "Sai";
 let age= 20;
 let hobbies = ['coding','reading','playing','eating', 'learning ' ]
+let job ={
+    title:'developer',
+    place:'newyork',
+    salary:'50000',
+    greet(){
+        alert('hello')
+    }
+}
+
+
+job.greet();
+alert(hobbies[0])
+alert(job.title)
+
+let a=10
+let b;
+function numb(c){
+     return c+18
+
+}
+b=numb(10);
+alert(b)
+a=15;
+b=numb(a);
+alert(b);
+
+
+
 console.log(hobbies[0] )
 console.log(hobbies[1] )
 console.log(hobbies[2] ) 
 console.log(hobbies[3] ) 
+console.log(age)
---
+++ operations.js
@@ -0,0 +1,22 @@
+console.log(10+4);
+console.log(10-4);
+console.log(10*4);
+console.log(10/4);
+console.log(10%4);
+
+
+let a=10;
+a++;//a=a+1
+a--;//a=a-1
+a+=5;//a=a+5
+a-=5;//a=a-5
+console.log(a);
+
+
+
+let user='DUDUKA';
+console.log(user.length);
+
+
+let hobby=['sport','cooking'];
+console.log(hobby.length)
